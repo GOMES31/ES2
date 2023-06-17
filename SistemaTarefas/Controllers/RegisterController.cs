7c93644d9e2c2c80c159825f830fdc7aa600f4f6
Eduardo Gomes <95891238+GOMES31@users.noreply.github.com>
2023-06-17 23:06:32 +0100

    updated

--- SistemaTarefas/Controllers/RegisterController.cs
+++ SistemaTarefas/Controllers/RegisterController.cs
@@ -1,10 +1,17 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
 using SistemaTarefas.Context;
 using SistemaTarefas.Entities;
 using SistemaTarefas.Models;
+using System.Text.Json;
 
 namespace SistemaTarefas.Controllers;
 
@@ -21,36 +28,61 @@ public class RegisterController : Controller
     {
         return View();
     }
-    
+
+
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Register([Bind("Name,Email,Password")] RegisterModel register)
+    public async Task<IActionResult> Register([Bind("Email,Password,Name")] RegisterModel register)
     {
-        if(!ModelState.IsValid) return View();
-        
-        // Verificar se o email já está a ser usado por outro user
-        // TODO - SUBSTITUIR ISTO PELA API
-        var existingUser = _context.Users.FirstOrDefault(u => u.email.Equals(register.Email));
-        if (existingUser != null)
+        if (!ModelState.IsValid)
         {
-            ModelState.AddModelError("Email", "O email já está em uso.");
-            return View(register);
+            return View();
         }
 
-        // Criar um novo objeto de user com os dados do registo
-        var newUser = new User
+        try
         {
-            email = register.Email,
-            password = register.Password,
-            name = register.Name
-        };
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:5001");
+                HttpResponseMessage response =  httpClient.GetAsync("api/WebAPI/userEmails").Result;
+                
+                Debug.WriteLine(response);
 
-        // Guardar o novo utilizador na base de dados
-        _context.Users.Add(newUser);
-        await _context.SaveChangesAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    ViewBag.ErrorMessage = "An error occurred while processing the request.";
+                    return View();
+                }
 
-        // Redirecionar para a página inicial
-        return RedirectToAction("Index","Home");
+                List<string> userEmails = await response.Content.ReadFromJsonAsync<List<string>>().ConfigureAwait(false);
+
+                //Check if the register.Email already exists in the userEmails list
+                if (userEmails.Contains(register.Email))
+                {
+                    ModelState.AddModelError("Email", "O email já está em uso.");
+                    return View();
+                }
+
+                // Create a new object of User with the registration data
+                User newUser = new User(register.Email, register.Password, register.Name);
+
+                // Save the new user to the database
+                _context.Users.Add(newUser);
+                await _context.SaveChangesAsync().ConfigureAwait(false);
+
+                UserSession.UserId = newUser.idUser;
+                UserSession.Username = newUser.name;
+
+                // Redirect to the home page
+                return RedirectToAction("Index", "Home");
+            }
+        }
+            
+        catch (Exception ex)
+        {
+            ViewBag.ErrorMessage = "An error occurred while processing the request.";
+            return View();
+        }
     }
 
 }  
