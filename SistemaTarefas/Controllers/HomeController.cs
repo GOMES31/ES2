6c790aee80881f881c2c082616e562549e52d193
Eduardo Gomes <95891238+GOMES31@users.noreply.github.com>
2023-06-18 15:06:48 +0100

    commit

--- SistemaTarefas/Controllers/HomeController.cs
+++ SistemaTarefas/Controllers/HomeController.cs
@@ -30,16 +30,6 @@ namespace SistemaTarefas.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Clients()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
