30b46484acc9a838d12260bafbda90fdd4db3e2f
Eduardo Gomes <95891238+GOMES31@users.noreply.github.com>
2023-06-18 21:50:17 +0100

    commit

--- SistemaTarefas/Startup.cs
+++ SistemaTarefas/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -30,6 +31,9 @@ namespace SistemaTarefas
             #else
             services.AddControllersWithViews();
             #endif
+            
+            services.AddSession();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             services.AddDbContext<SistemaDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
