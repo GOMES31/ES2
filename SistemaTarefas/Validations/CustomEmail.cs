4578f29d8c673c7c4a3d624896847e1f88822a76
Eduardo Gomes <95891238+GOMES31@users.noreply.github.com>
2023-06-18 15:12:12 +0100

    c

---
+++ SistemaTarefas/Validations/CustomEmail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SistemaTarefas.Validations;
+
+public class CustomEmail : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null)
+            return true;
+
+        string email = value.ToString();
+        const string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(email);
+    }
+}
